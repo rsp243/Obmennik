562ef871b4c410f14081f81e6d56978a2f257eb5
rsp243 <reshetov.semjon@gmail.com>
2021-04-21 22:26:30 +0400

    Fixed Amout and coinName and recieveName

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-$('#validationServer01').on('focus', function() {
+$('#take').on('blur', function() {
     let takeCoin = document.getElementById('take').value;
     let giveCoin = document.getElementById('give').value;
     if (takeCoin != '' && giveCoin != ''){  
         sl1 = JSON.stringify({"coinName": takeCoin, "receiveName": giveCoin})
         console.log(sl1)
     }
-    $('#validationServer01').on('blur', function() {
-    let count = document.getElementById('validationServer01').value;
-    if (takeCoin != '' && giveCoin != '' && count != ''){  
-        sl2 = JSON.stringify({"payment": Number(count)})
-        console.log(sl2)
-    }
-})
 })
 
+$('#give').on('blur', function() {
+    let takeCoin = document.getElementById('take').value;
+    let giveCoin = document.getElementById('give').value;
+    if (takeCoin != '' && giveCoin != ''){  
+        sl1 = JSON.stringify({"coinName": takeCoin, "receiveName": giveCoin})
+        console.log(sl1)
+    }
+})
+
+$('#validationServer01').on('blur', function() {
+    let takeCoin = document.getElementById('take').value;
+    let giveCoin = document.getElementById('give').value;
+    let count = document.getElementById('validationServer01').value;
+    if (takeCoin != '' && giveCoin != '' && count != ''){  
+        count = Number(count)
+        if (isNaN(count)){
+            $('#error').text('Введите корректные данные!')
+        } else {
+            $('#error').text('')
+            sl2 = JSON.stringify({"amount": Number(count)})
+            console.log(sl2)
+        }
+        
+    }
+})
