449099913849eacdaf1d7a2e37153697a41e288d
rsp243 <reshetov.semjon@gmail.com>
2021-04-21 22:06:06 +0400

    2 req

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-$('#validationServer01').on('blur', function() {
+$('#validationServer01').on('focus', function() {
     let takeCoin = document.getElementById('take').value;
     let giveCoin = document.getElementById('give').value;
-    let count = document.getElementById('validationServer01').value;
     if (takeCoin != '' && giveCoin != ''){  
-        sl = JSON.stringify({"coinName": takeCoin, "receiveName": giveCoin})
-        console.log(sl)
+        sl1 = JSON.stringify({"coinName": takeCoin, "receiveName": giveCoin})
+        console.log(sl1)
+    }
+    $('#validationServer01').on('blur', function() {
+    let count = document.getElementById('validationServer01').value;
+    if (takeCoin != '' && giveCoin != '' && count != ''){  
+        sl2 = JSON.stringify({"payment": Number(count)})
+        console.log(sl2)
     }
 })
+})
+
